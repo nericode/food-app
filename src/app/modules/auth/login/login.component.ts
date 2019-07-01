04d3508fe68f682edebcef5089745c365c21cd00
nericodec <alanneri10@gmail.com>
2019-07-01 18:48:55 -0500

    maquetado sign in

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,7 +1,12 @@
 import { Component } from "@angular/core";
+import { Page } from "tns-core-modules/ui/page/page";
 
 @Component({
     moduleId: module.id,
     templateUrl: "./login.component.html"
 })
-export class LoginComponent {}
+export class LoginComponent {
+    constructor(private page: Page) {
+        this.page.actionBarHidden = true;
+    }
+}
